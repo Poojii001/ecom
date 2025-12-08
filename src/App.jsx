e37e8ec4fad1a16bd08502f3d38470589e6f7610
Poojii001 <poojapal5781@gmail.com>
2025-12-08 21:07:23 +0530

    ecom

--- src/App.jsx
+++ src/App.jsx
@@ -16,30 +16,34 @@ import PrivicyPolicyPage from './Pages/Policies/PrivicyPolicyPage'
 import TermsAndConditions from './Pages/Policies/TermsAndConditions'
 
 import SignupPage from './Pages/User/SignupPage'
+import LoginPage from './Pages/User/LoginPage'
+import ProfilePage from './Pages/User/ProfilePage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-          <Routes>
-              <Route path='' element={<HomePage/>}/>
-              <Route path='/about' element={<AboutPage/>}/>
-              <Route path='/features' element={<FeaturesPage/>}/>
-              <Route path='/shop' element={<ShopPage/>}/>
-              <Route path='/product/:id' element={<ProductPage/>}/>
-              <Route path='/testimonial' element={<TestimonialPage/>}/>
-              <Route path='/faq' element={<FaqPage/>}/>
-              <Route path='/contactus' element={<ContactUsPage/>}/>
+      <Routes>
+        <Route path='' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/features' element={<FeaturesPage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/product/:id' element={<ProductPage />} />
+        <Route path='/testimonial' element={<TestimonialPage />} />
+        <Route path='/faq' element={<FaqPage />} />
+        <Route path='/contactus' element={<ContactUsPage />} />
 
-              <Route path='/privicy-policy' element={<PrivicyPolicyPage/>}/>
-              <Route path='/terms-conditions' element={<TermsAndConditions/>}/>
+        <Route path='/privicy-policy' element={<PrivicyPolicyPage />} />
+        <Route path='/terms-conditions' element={<TermsAndConditions />} />
 
-              <Route path='/signup' element={<SignupPage/>}/>
-              
-              <Route path='/*' element={<Error404Page/>}/>
-              
-          </Routes>
-      <Footer/>
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+
+        <Route path='/*' element={<Error404Page />} />
+
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
