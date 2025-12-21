52ded54093604b5c6d07da113b2203362d793583
Poojii001 <poojapal5781@gmail.com>
2025-12-21 21:11:19 +0530

    Reduc-Sagas-services

--- src/Redux/Sagas/services/index.jsx
+++ src/Redux/Sagas/services/index.jsx
@@ -35,3 +35,76 @@ export async function createMultipartRecord(collection, payload) {
         return []
     }
 }
+
+
+export async function getRecord(collection,payload) {
+    try {
+        let response = await fetch(`${import.meta.env.VITE_APP_BACKEND_SERVER}/${collection}`, {
+            method : "GET",
+            headers : {
+                "content-type": "application/json"
+            },
+            body : JSON.stringify(payload)
+        })
+        response = await response.json()
+        return response
+    } catch (error) {
+        console.log(`Error In API Calling Service : ${error}`)
+        return []
+    }
+}
+
+
+export async function updateRecord(collection, payload) {
+    try {
+        let response = await fetch(`${import.meta.env.VITE_APP_BACKEND_SERVER}/${collection}/${payload.id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+        response = await response.json()
+        return response
+    }
+    catch (error) {
+        console.log(`Error In API Calling Service : ${error}`)
+        return []
+    }
+}
+
+
+//use this function if payload contains any file field like image
+export async function updateMultipartRecord(collection, payload) {
+    try {
+        let response = await fetch(`${import.meta.env.VITE_APP_BACKEND_SERVER}/${collection}/${payload.get}`, {
+            method: "PUT",
+            headers: {
+            },
+            body: payload
+        })
+        response = await response.json()
+        return response
+    }
+    catch (error) {
+        console.log(`Error In API Calling Service : ${error}`)
+        return []
+    }
+}
+
+export async function deleteRecord(collection, payload) {
+    try {
+        let response = await fetch(`${import.meta.env.VITE_APP_BACKEND_SERVER}/${collection}/${payload.id}`, {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+        response = await response.json()
+        return response
+    }
+    catch (error) {
+        console.log(`Error In API Calling Service : ${error}`)
+        return []
+    }
+}
